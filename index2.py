22ab4278309895338cab6be8429296d87ec725a7
Melquiades998 <69167799+Melquiades998@users.noreply.github.com>
2023-11-15 23:06:13 -0500

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,39 @@
+from telegram import (
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    Update, 
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove)
+from telegram.ext import (
+    ApplicationBuilder, 
+    CommandHandler, 
+    ContextTypes, 
+    CallbackQueryHandler, 
+    ConversationHandler,
+    MessageHandler,
+    filters,
+    BaseRateLimiter)
+import logging
+import asyncio
+
+# Enable logging
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+async def echo (update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text
+    await context.bot.send_message(chat_id=update.effective_user.id, text=msg)
+    
+def main() -> None:
+    app = ApplicationBuilder().token("1925032512:AAGk0jyH2trNcq6MQOCvhV1yBwvft466Q3U").build()
+
+    # Add handler
+    app.add_handler(MessageHandler(filters.Text(), echo))
+
+    # Run app
+    app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
